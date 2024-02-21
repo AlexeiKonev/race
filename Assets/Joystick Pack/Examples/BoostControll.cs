9669302790c22d7b9e64dab694a294bd74d3fe00
AlexeiKonev <alexkonev777@gmail.com>
2024-02-21 11:43:42 +0300

    init

---
+++ Assets/Joystick Pack/Examples/BoostControll.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostControll : MonoBehaviour
+{
+    GameObject back;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public float speedForward = 0.7f;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+    public GameObject joisticBackground;
+
+    public void FixedUpdate()
+    {
+        if (joisticBackground.activeSelf)
+        {
+            Vector3 direction = Vector3.forward
+           * speedForward /*variableJoystick.Vertical*/
+           + Vector3.right
+           * variableJoystick.Horizontal;
+
+            rb.AddForce(direction
+                * speed
+                * Time.fixedDeltaTime,
+                ForceMode.VelocityChange);
+        }
+       
+
+      
+    }
+}
